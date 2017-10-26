da414a218c049ceb71c157278270b5d22832a930
Ariel Montenegro <montenegroariel@gmail.com>
2017-10-26 12:32:33 -0300

    modify test

--- test.py
+++ test.py
@@ -4,27 +4,7 @@
 from encode import encode_msg, encode_phn
 from send_message import send_message
 
-number = '3704 - 713781 '
-phone = number.replace('-','')
-phone = phone.replace(' ','')
-
-
-
-x='aaa12333bb445bb54b5b52'
-import string
-all=string.maketrans('','')
-nodigs=all.translate(all, string.digits)
-a= x.translate(all, nodigs)
-print(a)
-
-import urllib2
-#urllib2.urlopen('http://10.91.2.56:8009/?3704713781:21')
-#phone = number.strip()
-#print(phone)
-
-
-phone = encode_phn('3704713781')
-message = encode_msg('a')
-#send_message(phone,message)
-send_message('+543704713781','á')
+phone = encode_phn('3704713782')
+message = encode_msg('hello world')
+send_message(phone,message)
 
